7784accdf2ea64e06abaebfcc26cb7b46e46376d
asher <ashtonday09@gmail.com>
2024-02-27 13:32:40 -0600

    Fixed Method Issues

--- Assign1UnitTests/UnitTest1.cs
+++ Assign1UnitTests/UnitTest1.cs
@@ -1,38 +1,94 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Hello_World;
+using System;
 
-namespace Assign1UnitTests
+namespace Hello_World.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class UnitTest1
     {
-        [DataTestMethod]
-        [DataRow("6")]
-        [DataRow("-1")]
-        public void InvalidNumber(string input)
+        [TestMethod]
+        public void PrintMenuTest()
         {
-            var hello = new Hello_World.Program();
+
             var menu = "\n1: Select Random Number 1-10\n" +
                     "2: Print Current Date in ShortDate format\n" +
                     "3: Sort and print random Name of 10 Dinos\n" +
                     "4: Random String Manipulation\n\n";
             using (var writer = new StringWriter())
             {
-                
-                using (var reader = new StringReader(input))
-                {
-                    Console.SetOut(writer);
-                    Console.SetIn(reader);
-
-                    string yea = writer.ToString();
-                    Assert.AreEqual(menu, writer.ToString());
-                }
+                Console.SetOut(writer);
+                Functions.PrintMenu();
+                string yea = writer.ToString();
+                Assert.AreEqual(menu, writer.ToString());
             }
         }
         [TestMethod]
-        public void TestMethod1()
+        public void RandomNumber()
         {
+            for (var i = 0; i < 100; i++)
+            {
+                var num = Functions.RandomNumber();
+                Assert.IsTrue(num >= 1 && num < 11);
+            }
 
+        }
+
+        [TestMethod]
+        public void CheckShortDate()
+        {
+            string date = Functions.PrintDate();
+
+            Assert.AreEqual(DateTime.Now.ToShortDateString(), date);
+        }
+
+        [TestMethod]
+        public void CheckDinoSort()
+        {
+            List<string> dinos = new List<string>()
+                    {
+                        "Hypacrosaurus",
+                        "Aeolosaurus",
+                        "Zanabazar",
+                        "Alamosaurus",
+                        "Nuthetes",
+                        "Szechuanosaurus",
+                        "Torvosaurus",
+                        "Vulcanodon",
+                        "Acrotholus",
+                        "Pampadromaeus",
+                    };
+
+            var finalres = dinos.OrderBy(n => n).ToList<string>();
+            for (var x = 0; x < 100; x++)
+            {
+                bool check = false;
+                string output = Functions.RandomDinoSort();
+                for (int i = 0; i < finalres.Count; i++)
+                {
+                    if (output == finalres[i].ToString() + " index " + i)
+                    {
+                        check = true;
+                    }
+                }
+                Assert.IsTrue(check);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")]
+        [DataRow("exmaple strings yeah")]
+        [DataRow("\n\n\n\n\n\n\n;;;;...sdsd")]
+        [DataRow("\n")]
+        [DataRow("                                                ")]
+        public void CheckRandomStringManipulation(string input)
+        {
+            for (var x = 0; x < 100; x++)
+            {
+                var output = Functions.RandomStringManipulation(input);
+                Assert.IsNotNull(output);
+            }
         }
     }
 }
