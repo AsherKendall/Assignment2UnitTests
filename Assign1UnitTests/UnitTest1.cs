f14ae12e01b6fc2d199f991e1bcb6b36f269aae7
asher <ashtonday09@gmail.com>
2024-02-27 12:52:09 -0600

    Create Methods

--- Assign1UnitTests/UnitTest1.cs
+++ Assign1UnitTests/UnitTest1.cs
@@ -6,9 +6,33 @@ namespace Assign1UnitTests
     [TestClass]
     public class UnitTest1
     {
+        [DataTestMethod]
+        [DataRow("6")]
+        [DataRow("-1")]
+        public void InvalidNumber(string input)
+        {
+            var hello = new Hello_World.Program();
+            var menu = "\n1: Select Random Number 1-10\n" +
+                    "2: Print Current Date in ShortDate format\n" +
+                    "3: Sort and print random Name of 10 Dinos\n" +
+                    "4: Random String Manipulation\n\n";
+            using (var writer = new StringWriter())
+            {
+                
+                using (var reader = new StringReader(input))
+                {
+                    Console.SetOut(writer);
+                    Console.SetIn(reader);
+
+                    string yea = writer.ToString();
+                    Assert.AreEqual(menu, writer.ToString());
+                }
+            }
+        }
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
