f336672975b35ddb83a7516be3c683cb5ef649e3
asher <ashtonday09@gmail.com>
2024-02-26 19:09:46 -0600

    Added Basic Files

---
+++ Assign1UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hello_World;
+
+namespace Assign1UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
